130d81229219220113e4bd1b042169002428266a
yinmay <1102316727@qq.com>
2020-05-04 13:44:51 +0800

    命名隔离

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,16 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
+    autoprefixer: {},
+    'postcss-selector-namespace': {
+      namespace(css) {
+        console.log(css);
+        return '.gui-vue';
+      },
+    },
+  },
+};
